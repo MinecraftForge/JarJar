f8f1749d99e0e90d99a3f31440ebc6b3b0835fd2
Marc Hermans <marc.hermans@ldtteam.com>
2022-05-23 19:29:56 +0200

    Remove the not needed hash.

--- src/main/java/net/minecraftforge/spi/jij/ContainedJar.java
+++ src/main/java/net/minecraftforge/spi/jij/ContainedJar.java
@@ -2,6 +2,6 @@ package net.minecraftforge.spi.jij;
 
 import org.apache.maven.artifact.versioning.VersionRange;
 
-public record ContainedJar(ContainedJarIdentifier identifier, VersionRange version, String sha256Hash, String path)
+public record ContainedJar(ContainedJarIdentifier identifier, VersionRange version, String path)
 {
 }
